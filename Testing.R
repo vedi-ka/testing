574f243dd00292e80f1dc320a5c7071d2ee3955f
vedi-ka <vedikawangikar@gmail.com>
2020-06-06 18:43:06 +0800

    Updating HelloWorld

--- Testing.R
+++ Testing.R
@@ -2,3 +2,10 @@ print("This file was created within RStudio")
 
 print("And now it lives on GitHub")
 
+update.packages(ask = FALSE, checkBuilt = TRUE)  # update R packages
+tinytex::tlmgr_update()  # update LaTeX packages
+options(tinytex.verbose = TRUE)
+
+install_tinytex()
+
+
